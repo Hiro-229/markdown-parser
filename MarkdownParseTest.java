438f96ab34233fa5092fb24cb30e73e9189805d9
Hiro-229 <ntojima@ucsd.edu>
2022-04-22 11:09:23 -0700

    Test file added
    Command lines used to run it:
    javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*; //import the needed Junit assert class
+import org.junit.*; //import the org.Junit class files
+public class MarkdownParseTest { //class header/new class creation
+    @Test //singlas there is a new test
+    public void addition() { //test method created
+        assertEquals(2, 1 + 1); //compares the expected with the actual, 
+                                        //and will only work if they match
+    }
+}
